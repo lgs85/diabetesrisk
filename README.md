47b77ed5d2ea64fba5386452db0e9403fc541a43
Lewis Spurgin <lewisspurgin@gmail.com>
2024-04-28 21:22:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# diabetesrisk
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+The goal of diabetesrisk is to …
+
+## Installation
+
+You can install the development version of diabetesrisk from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("lgs85/diabetesrisk")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(diabetesrisk)
+## basic example code
+```
--- dev/01_start.R
+++ dev/01_start.R
@@ -20,13 +20,13 @@
 ##
 golem::fill_desc(
   pkg_name = "diabetesrisk", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL, # The URL of the GitHub Repo (optional),
-  pkg_version = "0.0.0.9000" # The Version of the package containing the App
+  pkg_title = "Diabetes risk", # The Title of the package containing the App
+  pkg_description = "A dashboard for exploring diabetes risk scores.", # The Description of the package containing the App
+  author_first_name = "Lewis", # Your First Name
+  author_last_name = "Spurgin", # Your Last Name
+  author_email = "lewisspurgin@gmail.com", # Your Email
+  repo_url = "https://github.com/lgs85/diabetesrisk", # The URL of the GitHub Repo (optional),
+  pkg_version = "0.1.0" # The Version of the package containing the App
 )
 
 ## Set {golem} options ----
@@ -37,12 +37,12 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("Lewis Spurgin") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Lewis Spurgin")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
